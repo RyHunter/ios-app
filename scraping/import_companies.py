1e3782e2567a9efc8037dbae7731ac5f09df8cbc
Chris Albers <calbers191@gmail.com>
2019-07-11 20:31:05 -0400

    Added some scraping files.

---
+++ scraping/import_companies.py
@@ -0,0 +1,28 @@
+import csv
+import mysql.connector
+import config
+
+db = mysql.connector.connect(user=config.DATABASE['user'], 
+                              password=config.DATABASE['password'],
+                              host=config.DATABASE['host'],
+                              port=config.DATABASE['port'],
+                              database=config.DATABASE['database'])
+cursor = db.cursor()
+
+insertQuery = ("INSERT INTO comp (url, name, twitter_handle) VALUES (%s, %s, %s)")
+
+file = open('Fortune-1000-Company-Twitter-Accounts.csv')
+
+companyAccountInfoCsv = csv.DictReader(file)
+
+for row in companyAccountInfoCsv:
+    url = row['domain']
+    name = row['name']
+    twitter_handle = row['twitter']
+
+    companyData = (url, name, twitter_handle)
+    cursor.execute(insertQuery, companyData)
+
+db.commit()
+cursor.close()
+db.close()
