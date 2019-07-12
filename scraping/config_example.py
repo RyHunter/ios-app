47b9190f2cbb877fde2bdf4351d0ed743ab5dd55
Chris Albers <calbers191@gmail.com>
2019-07-11 20:42:14 -0400

    Config example

---
+++ scraping/config_example.py
@@ -0,0 +1,7 @@
+DATABASE = {
+    'host': 'ios-summer-2019.c7rm7mi2tqkz.us-east-2.rds.amazonaws.com',
+    'database': 'ios_summer_2019',
+    'user': 'enter_db_username',
+    'port': 3305,
+    'password': 'enter_db_password',
+}
