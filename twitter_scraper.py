6c4dfe55facdef71c16a5750996508f0bbec34e6
RyHunter <k.kalashnikova@gmail.com>
2019-07-26 16:01:34 -0400

    Update twitter_scraper.py

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -6,6 +6,7 @@ import csv
 import tweepy
 #reading and writing from db
 import mysql.connector
+import time
 
 #twitter api creds
 consumer_key = ""
@@ -26,40 +27,46 @@ def connect(username, password):
 	except Exception as ex:
 		print(ex)
     	
+#insert tweets into db    	
 def insertPost(companyId, source, message, likesCount, commentsCount, retweetsCount, createdTime):
 	try:
-		insertQuery = ("INSERT INTO posts (company_id, source, content, likes_count, comments_count, retweets_count, created_time) VALUES (%s, %s, %s, %s, %s)")
+		insertQuery = ("INSERT INTO posts (company_id, source, content, likes_count, comments_count, retweets_count, created_time) VALUES (%s, %s, %s, %s, %s, %s, %s)")
 		postData = (companyId, source, message, likesCount, commentsCount, retweetsCount, createdTime)
+		print(postData)
 		db.cursor().execute(insertQuery, postData)
+		db.commit()
+		print("inserted row")
 	except Exception as ex:
 		print(ex)
         	
 #get last tweets
-def get_tweets(username):
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_key, access_secret)
-	api = tweepy.API(auth)
-	
-	#set tweet count
-	number_of_tweets = 5
-	
-	#get tweets
-	tweets_for_csv = []
-	for tweet in tweepy.Cursor(api.user_timeline, screen_name = username, include_rts = False, tweet_mode="extended").items(number_of_tweets):
-	#array of tweet info
-		if tweet.in_reply_to_status_id is None:
-			insertPost(screen_name, 'Twitter', tweet.full_text.encode("utf-8"), tweet.favorite_count, 'null', tweet.retweet_count, tweet.created_at)
-			#tweets_for_csv.append([username, tweet.created_at, tweet.full_text.encode("utf-8"), tweet.retweet_count, tweet.favorite_count]) 
+def get_tweets(user):
+	try:
+		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_key, access_secret)
+		api = tweepy.API(auth)
 		
-	#outfile = username + "_tweets.csv"
-	#print ("writing to " + outfile)
-	#with open(outfile, 'w+') as file:
-	#		writer = csv.writer(file, delimiter = ',')
-	#		writer.writerows(tweets_for_csv) 
+		username = user[0]
+		userId = user[1]
+		source = 'Twitter'
+		
+		#set tweet count
+		number_of_tweets = 5
+		#get tweets
+		for tweet in tweepy.Cursor(api.user_timeline, screen_name = username, include_rts = False, tweet_mode="extended").items(number_of_tweets):
+		#array of tweet info
+			if tweet.in_reply_to_status_id is None:
+				ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(str(tweet.created_at),'%Y-%m-%d %H:%M:%S'))
+				insertPost(userId, source, tweet.full_text.encode("utf-8"), tweet.favorite_count, 0, tweet.retweet_count, ts)
+			else:
+				print("@tweet excluded")
+
+	except Exception as ex:
+		print(ex)
 			
 def selectUsers(cursor):
 	try: 
-		selectQuery = ("SELECT DISTINCT twitter_handle FROM companies")
+		selectQuery = ("SELECT DISTINCT twitter_handle, id FROM companies")
 		cur = cursor.execute(selectQuery)
 		users = list(cursor.fetchall())
 		return(users)
@@ -70,16 +77,11 @@ if __name__ == '__main__':
 	try:
 		db = connect('', '')
 		users = selectUsers(db.cursor())
-		for user in users:
-			print (user)
+		get_tweets(users[1])
+		#for user in users:
+			#get_tweets(user)
 	except Exception as ex: 
 		print(ex)
-	
-	
-#	if len(sys.argv) == 2:
-#		get_tweets(sys.argv[1])
-#	else:
-#		print ("error: enter username")
 		
 		
 		
