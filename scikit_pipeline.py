757c2f98a652f20797bc93d7c546d54267711682
Chris Albers <calbers191@gmail.com>
2019-08-04 16:05:56 -0400

    demo pipeline

--- scikit_pipeline.py
+++ scikit_pipeline.py
@@ -11,8 +11,9 @@ import database.db as db
 
 dbSelect = db.connect(config.DATABASE['user'], config.DATABASE['password'])
 
-data = pd.read_sql_query("""SELECT  p.id, 
-		p.impressions_ratio,       
+X_train = pd.read_sql_query("""
+        SELECT  
+        p.id, 
 		wc.noun_fraction, 
 		wc.pronoun_fraction, 
 		wc.verb_fraction, 
@@ -23,16 +24,50 @@ data = pd.read_sql_query("""SELECT  p.id,
         s.polarity    
         FROM posts p
         JOIN word_count wc ON p.id=wc.post_id
-        JOIN sentiment s ON p.id=s.post_id;
+        JOIN sentiment s ON p.id=s.post_id
+        WHERE p.created_time < "2019-01-01"
+        ORDER BY p.created_time DESC;
         """, dbSelect, "id")
 
-y = data.impressions_ratio
-X = data.drop('impressions_ratio', axis=1)
+X_test = pd.read_sql_query("""
+        SELECT  
+        p.id, 
+		wc.noun_fraction, 
+		wc.pronoun_fraction, 
+		wc.verb_fraction, 
+		wc.adjective_fraction, 
+		wc.adverb_fraction, 
+		wc.total_words,
+        s.afinn_sentiment,
+        s.polarity    
+        FROM posts p
+        JOIN word_count wc ON p.id=wc.post_id
+        JOIN sentiment s ON p.id=s.post_id
+        WHERE p.created_time >= "2019-01-01"
+        ORDER BY p.created_time DESC;
+        """, dbSelect, "id")
 
-print ("Splitting test set...")
-X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                    test_size=0.2, 
-                                                    random_state=123)
+y_train =pd.read_sql_query("""
+        SELECT     
+        p.id, 
+		p.impressions_ratio     
+        FROM posts p
+        WHERE p.created_time < "2019-01-01"
+        ORDER BY p.created_time DESC;
+        """, dbSelect, "id")
+
+y_train = y_train.impressions_ratio
+
+y_test =pd.read_sql_query("""
+        SELECT  
+        p.id, 
+		p.impressions_ratio     
+        FROM posts p
+        WHERE p.created_time >= "2019-01-01"
+        ORDER BY p.created_time DESC;
+        """, dbSelect, "id")
+
+y_test = y_test.impressions_ratio
 
 print ("Preprocessing...")
 scaler = preprocessing.StandardScaler().fit(X_train)
@@ -43,7 +78,7 @@ print ("Tuning hyperparameters...")
 pipeline = make_pipeline(preprocessing.StandardScaler(), 
                          RandomForestRegressor(n_estimators=100))
 
-hyperparameters = { 'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],
+hyperparameters = {'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],
                   'randomforestregressor__max_depth': [None, 5, 3, 1]}
 
 print ("Training model...")
@@ -55,4 +90,16 @@ y_pred = clf.predict(X_test)
 rSquared = r2_score(y_test, y_pred)
 meanSquaredError = mean_squared_error(y_test, y_pred)
 
-print (rSquared, meanSquaredError)
+print ("Writing known values to csv...")
+y_test.to_csv("test_results.csv", header=True)
+results_file = open("test_results.csv", 'a')
+
+print ("Writing predicted values to csv...")
+results_file.write("predicted_impressions_ratio\n")
+for element in np.nditer(y_pred):
+    results_file.write(str(element))
+    results_file.write("\n")
+
+results_file.close()
+
+print ("R squared: {} Mean Squared Error: {}".format(rSquared, meanSquaredError))
